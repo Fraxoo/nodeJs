7dce0cdf8704a77b658237d7bdcda5bf60b51baf
Fraxoo <fraxo.officiel@gmail.com>
2025-10-14 13:15:03 +0200

    .

--- server-http/app.mjs
+++ server-http/app.mjs
@@ -1,11 +1,31 @@
-import http,{IncomingMessage,ServerResponse} from "http";
+import http from "http";
+import fs from "fs/promises";
 
-const server = http.createServer((req,res) => {
-    res.end();
+const server = http.createServer(async (req,res)=>{
+    // ex : public/index.html
+    // ex : public/admin/dashboard.html
+    const pathFile = "public"+ (req.url == "/"? "/index.html" : req.url);
+    console.log(pathFile);
+    fs.readFile(pathFile).then(body=>{
+        
+        res.write(body);
+    }).catch(error=>{
+        console.log(error);
+
+        res.statusCode = 404;
+
+        res.write("Erreur : Fichier inconnu\n");
+    }).finally(()=>{
+        res.end();
+    })
+
 });
 
-server.listen(3000,() => {
-    console.log("server listen on http://0.0.0.0:3000");
+server.addListener("connection",(socket)=>{
     
+    console.log(`Connection detecté ! `,socket.remoteAddress);
 })
 
+server.listen(3000,()=>{
+    console.log("Server listen on http://0.0.0.0:3000");
+});
